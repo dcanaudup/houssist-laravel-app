ed132332871dacbfd813011a12feaa9cbf4caef9
dcanaud <darrylanaud@gmail.com>
2023-05-05 19:51:29 +0200

    Add advertisement filters in service provider

--- app/Http/Livewire/ServiceProvider/Advertisement/AdvertisementIndex.php
+++ app/Http/Livewire/ServiceProvider/Advertisement/AdvertisementIndex.php
@@ -7,15 +7,38 @@ use App\Http\Livewire\Traits\WithPerPagination;
 use App\Modules\HomeOwner\Enums\AdvertisementStatus;
 use App\Modules\Shared\Models\Advertisement;
 use Livewire\Component;
+use Spatie\Tags\Tag;
 
 class AdvertisementIndex extends Component
 {
     use WithCachedRows, WithPerPagination;
 
+    public $filters = [
+        'categories' => [],
+        'job_date_time' => null,
+    ];
+
+    public $categories = [];
+
+    public $selectedCategories = [];
+
+    public function mount()
+    {
+        $this->categories = \Spatie\LaravelOptions\Options::forModels(Tag::getWithType('jobCategory'))->toArray();
+    }
+
     public function getRowsQueryProperty()
     {
-        return Advertisement::query()
+        return Advertisement::join('taggables', function ($join) {
+                $join->on('taggables.taggable_id', '=', 'advertisements.advertisement_id')
+                    ->where('taggables.taggable_type', Advertisement::class);
+            })
             ->where('status', AdvertisementStatus::PENDING)
+            ->when($this->filters['categories'] ?? null, fn($query, $categories) => $query->whereIn('taggables.tag_id', $categories))
+            ->when($this->filters['job_date_time'] ?? null, fn($query, $jobDateTime) => $query->where(function ($q) use ($jobDateTime) {
+                $q->whereBetween('start_date_time', date_range_filter_transformer($jobDateTime))
+                    ->orWhereBetween('end_date_time', date_range_filter_transformer($jobDateTime));
+            }))
             ->with('media');
     }
 
@@ -32,4 +55,6 @@ class AdvertisementIndex extends Component
             'advertisements' => $this->rows,
         ]);
     }
+
+    public function resetFilters() { $this->reset('filters', 'selectedCategories'); }
 }
--- resources/views/livewire/service-provider/advertisement/advertisement-index.blade.php
+++ resources/views/livewire/service-provider/advertisement/advertisement-index.blade.php
@@ -1,9 +1,37 @@
 <div>
     <h1 class="text-4xl font-semibold text-gray-900">Available Ads</h1>
     <div class="py-4 space-y-4">
+        <!-- Top Bar -->
+        <div class="grid grid-cols-3 gap-2">
+            <div class="col-span-1">
+                <x-input.multi-select
+                    wire:model.lazy="filters.categories"
+                    id="categories"
+                    name="categories"
+                    placeholder="Filter by category..."
+                    label="Categories"
+                    :items="$categories"
+                    selected="selectedCategories"
+                >
+                </x-input.multi-select>
+            </div>
+            <div class="col-span-1 justify-center my-auto">
+                <x-search.date
+                    id="filters.job_date_time"
+                    name="filters.job_date_time"
+                    placeholder="Task Date"
+                    ref="job_date_time"
+                    dateFormat="Y-m-d"
+                    mode="range"
+                />
+            </div>
+            <div class="col-span-1 justify-center my-auto">
+                <x-button.link wire:click="resetFilters">Reset Filters</x-button.link>
+            </div>
+        </div>
         <div class="bg-white rounded-lg">
             <div class="mx-auto max-w-2xl py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 class="sr-only">Products</h2>
+                <h2 class="sr-only">Advertisements</h2>
 
                 <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     @forelse($advertisements as $advertisement)
